0578690154c80aae915d833f9fa1e670ed26971b
lucastavares06 <lucastavareswin10@outlook.com>
2025-04-10 23:30:00 -0300

    chore: remove unused import

--- src/vehicles/vehicles.c
+++ src/vehicles/vehicles.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "utils/utils.h"
 #include "vehicles/vehicles.h"
-#include "menu/menu.h"
 
 void add_vehicle(Vehicle fleet[], int *total_vehicles, Vehicle new_vehicle) {
 
