9b0636a919aa2fb2b69a0954217e4e1ee7b34db0
leofximenes <lximenes63@outlook.com>
2025-04-21 00:52:42 -0300

    feat: add fleet report and mileage update options

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -28,10 +28,10 @@ void show_menu(Vehicle fleet[], int *total_vehicles) {
                 check_vehicle(fleet, *total_vehicles);
                 break;
             case 3:
-                //to do//
+                generate_report(fleet, *total_vehicles);
                 break;
             case 4:
-                //to do//
+                update_mileage(fleet, *total_vehicles);
                 break;
             case 5:
                 println("Exiting...");
@@ -94,3 +94,51 @@ void check_vehicle(Vehicle fleet[], int total_vehicles) {
 
 }
 
+void generate_report(Vehicle fleet[], int total_vehicles) {
+
+    if (total_vehicles == 0) {
+        printf("No vehicles found !");
+        return;
+}
+    printf("\n=== List of Vehicles ===\n");
+
+    for (int i = 0; i < total_vehicles; i++) {
+        printf("\n#%d Vehicle:\n", i + 1);
+        printf("\n | TYPE: %s\n | PLATE: %s\n | MODEL: %s\n | YEAR: %d\n | MILEAGE: %d km\n",
+            get_vehicle_type(fleet[i].type), fleet[i].plate, fleet[i].model, fleet[i].year, fleet[i].mileage);
+    }
+
+}
+
+void update_mileage(Vehicle fleet[], int total_vehicles) {
+
+    char search_plate[9];
+    int found = 0;
+    int new_mileage = 0;
+
+    clear_input_buffer();
+
+    printf("Write the plate number from desired vehicle:\n");
+    read_string(search_plate, sizeof(search_plate));
+
+    clear_input_buffer();
+
+    for(int i = 0; i < total_vehicles; i++) {
+        if (strcmp(fleet[i].plate, search_plate) == 0) {
+            printf("\n Vehicle from plate === %s === found:\n", search_plate);
+            printf("\n Vehicle current mileage: %d km\n", fleet[i].mileage);
+
+            printf("\nType the new mileage\n");
+            scanf("%d", &new_mileage);
+
+            fleet[i].mileage = new_mileage;
+            printf("\n Mileage updated !\n");
+
+            found = 1;
+            break;
+        }
+    }
+        if (!found) {
+            printf("Vehicle from plate '%s' not found !", search_plate);
+        }
+    }
--- src/menu/menu.h
+++ src/menu/menu.h
@@ -7,5 +7,7 @@
 void register_vehicle(Vehicle fleet[], int *total_vehicles);
 void check_vehicle(Vehicle fleet[], int total_vehicles);
 void show_menu(Vehicle fleet[], int *total_vehicles);
+void generate_report(Vehicle fleet[], int total_vehicles);
+void update_mileage(Vehicle fleet[], int total_vehicles);
 
 #endif
